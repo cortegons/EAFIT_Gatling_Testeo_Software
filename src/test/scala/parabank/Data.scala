602829a57b42ff2bfb2d902e39a636242e9c1ca9
cortegons <88986744+cortegons@users.noreply.github.com>
2025-08-16 09:49:49 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,7 +4,10 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "testeo"
     val password = "123"
-
+    val amount =  "10"
+    val fromAccountId = "16785"
+    val toAccountId = "19338"
     
 }
+
 
