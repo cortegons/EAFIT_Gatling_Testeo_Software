6a02a22640a48231a6b2a6f577dafd4014b7e4b5
cortegons <88986744+cortegons@users.noreply.github.com>
2025-08-16 10:51:00 -0500

    Create TransferFunds.scala

---
+++ src/test/scala/parabank/TransferFunds.scala
@@ -0,0 +1,30 @@
+package parabank
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import parabank.Data._
+
+class TransferFunds extends Simulation{
+
+  // 1 Http Conf
+  val httpConf = http.baseUrl(url).acceptHeader("application/json")
+    //Verificar de forma general para todas las solicitudes
+  .check(status.is(200))
+
+  // 2 Scenario Definition
+  val scn = scenario("TransferFunds").
+    exec(http("TransferFunds")
+      .post("/transfer")
+      .queryParam("fromAccountId", fromAccountId)
+      .queryParam("toAccountId", toAccountId)
+      .queryParam("amount", amount)
+       //Recibir información de la cuenta
+      .check(status.is(200))
+    )
+
+  // 3 Load Scenario
+  setUp(
+    scn.inject(rampUsersPerSec(10).to(300).during(30))
+  ).protocols(httpConf);
+
+}
